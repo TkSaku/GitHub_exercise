5990271c7e4faf73efeaedf4d4ddc27fed23d19a
TkSaku <takuya.sakuyama@gmail.com>
2016-01-24 12:49:47 +0900

    Modified: mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,7 +1,7 @@
 <?php
-	$link = mysql_connect('localhost', 'root', '3196@Yuno');
+	$link = mysqli_connect('localhost', 'root', '3196@Yuno');
 	if(!$link) {
-		die('接続失敗!'.mysql_error()	);
+		die('接続失敗!<br>'.mysqli_connect_error()	);
 	} else {
 		echo '接続成功!';
 	}
